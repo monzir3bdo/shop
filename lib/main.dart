2d646b6fa3a15d8571fe5d889c78277542181788
Monzir Abdalrahman <monzir3bdo@gmail.com>
2022-05-12 01:33:10 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shop/view/screens/welcome_screen.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WelcomeScreen(),
+      initialRoute: '/WelcomeScreen',
+      getPages: [
+        GetPage(
+          name: '/WelcomeScreen',
+          page: () => const WelcomeScreen(),
+        ),
+      ],
+    );
+  }
+}
