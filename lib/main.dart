1308d5ff6c5a83c7f10f27aceb8a4f816a4b84c2
monzir3bdo <monzir3bdo@gmail.com>
2022-05-13 03:18:17 +0300

    I have added new section

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/routes/routes.dart';
 import 'package:shop/view/screens/welcome_screen.dart';
 import 'package:get/get.dart';
 
@@ -16,14 +17,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WelcomeScreen(),
-      initialRoute: '/WelcomeScreen',
-      getPages: [
-        GetPage(
-          name: '/WelcomeScreen',
-          page: () => const WelcomeScreen(),
-        ),
-      ],
+      initialRoute: AppRoute.welcome,
+      getPages: AppRoute.routes,
     );
   }
 }
